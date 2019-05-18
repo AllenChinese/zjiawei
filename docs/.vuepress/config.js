e778e5f579559dfe5b0b1f490f923905121e30fc
AllenChinese <13402202685@163.com>
2019-05-18 23:40:11 +0800

    feat: 发布文章《浅谈性能与优化》

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: '记录开发中的点点滴滴，攒出一本散文诗',
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
-    ['link', { rel: 'icon', href: '/logo.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/logo.ico' }] // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: '/', // 这是部署到github相关的配置 下面会讲
   markdown: {
@@ -39,14 +39,15 @@ module.exports = {
         {
           title: 'HTML',
           children: [
-            ['/blog/html/HTML—three-characters-achieve-chinese-alignment', '字符实现中文的等宽对齐效果']
+            [
+              '/blog/html/HTML—three-characters-achieve-chinese-alignment',
+              '字符实现中文的等宽对齐效果'
+            ]
           ]
         },
         {
           title: 'CSS',
-          children: [
-            ['/blog/css/comming', '待完善']
-          ]
+          children: [['/blog/css/comming', '待完善']]
         },
         {
           title: 'JavaScript',
@@ -59,69 +60,55 @@ module.exports = {
           title: 'Vue',
           children: [
             ['/blog/vue/vue-configurable-form', '结合 UI 框架实现可配置 Vue 表单组件浅析'],
-            ['/blog/vue/vue-cli2.0-demo', 'vue-cli 仿网易云音乐 Demo,环境搭建到开发 Vue 全家桶练手项目'],
+            [
+              '/blog/vue/vue-cli2.0-demo',
+              'vue-cli 仿网易云音乐 Demo,环境搭建到开发 Vue 全家桶练手项目'
+            ],
             ['/blog/vue/vue-nuxt-test', '基于 Vue.js 的服务端渲染 (ssr) 通用应用框架 Nuxt.js']
           ]
         },
         {
           title: 'React',
-          children: [
-            ['/blog/react/comming', '待完善']
-          ]
+          children: [['/blog/react/comming', '待完善']]
         },
         {
           title: 'Node.js',
-          children: [
-            ['/blog/nodejs/comming', '待完善']
-          ]
+          children: [['/blog/nodejs/comming', '待完善']]
         },
         {
           title: '小程序',
-          children: [
-            ['/blog/miniapp/comming', '待完善']
-          ]
+          children: [['/blog/miniapp/comming', '待完善']]
         },
         {
           title: '动画',
-          children: [
-            ['/blog/animation/a-lottie', 'airbnb Lottie 动画库实践']
-          ]
+          children: [['/blog/animation/a-lottie', 'airbnb Lottie 动画库实践']]
         },
         {
           title: '前端整理',
-          children: [
-            ['/blog/web-package/base-skill', '前端基础技能']
-          ]
+          children: [['/blog/web-package/base-skill', '前端基础技能']]
         },
         {
           title: '性能优化',
           children: [
-            ['/blog/performance/p--vue-cli-f2e-performance', 'vue-cli 前端性能优化方案']
+            ['/blog/performance/p--vue-cli-f2e-performance', 'vue-cli 前端性能优化方案'],
+            ['/blog/performance/p--RAIL', '浅谈性能标准与优化']
           ]
         },
         {
           title: '架构 & 解决方案',
-          children: [
-            ['/blog/architecture/web-page-configuration', '前端页面「配置化」实践']
-          ]
+          children: [['/blog/architecture/web-page-configuration', '前端页面「配置化」实践']]
         },
         {
           title: '管理',
-          children: [
-            ['/blog/manager/m-a-project-life-circle', '一个项目的生命周期']
-          ]
+          children: [['/blog/manager/m-a-project-life-circle', '一个项目的生命周期']]
         },
         {
           title: '我的项目',
-          children: [
-            ['/blog/myProject/wx-mark', '微信小程序「Mark」文档']
-          ]
+          children: [['/blog/myProject/wx-mark', '微信小程序「Mark」文档']]
         },
         {
           title: '踩坑之旅',
-          children: [
-            ['/blog/done/do-html2canvas', 'html2canvas 踩坑之旅']
-          ]
+          children: [['/blog/done/do-html2canvas', 'html2canvas 踩坑之旅']]
         }
       ],
       '/essay': [
@@ -129,4 +116,4 @@ module.exports = {
       ]
     }
   }
-};
+}
